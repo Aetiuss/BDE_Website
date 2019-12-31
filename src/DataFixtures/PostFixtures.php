507288ae5e7e6a11b7a6b09ec60f6371a7b0b9d6
jules <jules.bader@viacesi.fr>
2019-12-31 14:35:13 +0100

    Post fixture

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -4,14 +4,22 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Post;
+use DateTime;
 
 class PostFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 10; $i++) {
+            $article = new Post();
+            $article->setTitle("Titre de l'article n°$i")
+                ->setContent("<p>Contenu de l'article n°$i<p>")
+                ->setPicture("http://placehold.it/350x150")
+                ->setCreatedAt(new \DateTime());
 
+            $manager->persist($article);
+        }
         $manager->flush();
     }
 }
