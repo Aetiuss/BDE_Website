5b4a38ae7be402c4effebe8656f17fc05d845212
jules <jules.bader@viacesi.fr>
2020-01-18 14:18:53 +0100

    Added ReportType & vue

--- src/Migrations/Version20200118131502.php
+++ src/Migrations/Version20200118131502.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200118125032 extends AbstractMigration
+final class Version20200118131502 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,7 @@ final class Version20200118125032 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE role CHANGE id id VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles_id roles_id VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD is_banned TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -31,7 +30,6 @@ final class Version20200118125032 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE role CHANGE id id INT AUTO_INCREMENT NOT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles_id roles_id INT NOT NULL');
+        $this->addSql('ALTER TABLE user DROP is_banned');
     }
 }
